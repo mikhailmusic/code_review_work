c3218038317329fcb58707d2ac4e3fa6f064e742
Mikhail Alekseev <mishaalekseev2004@mail.ru>
2024-11-28 23:34:34 +0300

    add Calculator

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,20 @@
 public class Calculator {
     public int add(int a, int b){
-        //TODO inser your realisation in method add
+        return a + b;
     }
     public int dif(int a, int b){
-        //TODO inser your realisation in method dif
+        return a - b;
     }
     public int div(int a, int b){
-        //TODO inser your realisation in method div
+        if (b == 0) {
+            throw new ArithmeticException("Деление на ноль недопустимо");
+        }
+        return a / b;
     }
     public int times(int a, int b){
-        //TODO inser your realisation in method times
+        return a * b;
     }
     public int solver(){
-        //TODO inser your realisation in method solver
+        return div(times(add(10, 5), dif(15, 3)), 4);
     }
 }
